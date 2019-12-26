890ad9abd8b6d57ab861e8b85cf5289b9d183d07
tjorvuk <59225226+tjorvuk@users.noreply.github.com>
2019-12-26 10:27:06 +0100

    Update run.py

--- run.py
+++ run.py
@@ -6,6 +6,6 @@ def dispatch(func, delta, freq, args = [], window=100, dbg=False):
 def main(load_module, delta, run_command):
     # print('elapsed:', delta)
 
-    dispatch(lambda: load_module('hello').main(), delta, 1)
-    dispatch(lambda: print('-----'), delta, 60 * 1000)
-    dispatch(lambda: run_command('update'), delta, 60 * 5 * 1000, dbg=True)
+    # dispatch(lambda: load_module('hello').main(), delta, 1)
+    dispatch(lambda: print('-----'), delta, 1 * 1000)
+    dispatch(lambda: run_command('update'), delta, 10 * 1 * 1000, dbg=True)
